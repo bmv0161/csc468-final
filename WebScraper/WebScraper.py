fe93faf86807d5cd75029943d3b2de6d2f50a552
bmv0161 <bmv0161@gmail.com>
2022-04-05 21:16:57 -0400

    functioning scraper for ramconnect

--- WebScraper/WebScraper.py
+++ WebScraper/WebScraper.py
@@ -1,12 +1,32 @@
-import pandas as pd
 import requests
+import json
 from bs4 import BeautifulSoup
 
-page = requests.get('https://ramconnect.wcupa.edu/events')
+payload = {'range': '0', 'limit': '40'}
+url = 'https://ramconnect.wcupa.edu/mobile_ws/v17/mobile_events_list'
+#header = {'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
+response = requests.get(url, params=payload)
+#print(response.status_code)
+data = response.json()[1:]
+fields = {}
+keys = data[0]['fields'].split(',')[:-1]
 
-soup = BeautifulSoup(page.content, 'html.parser')
-html = list(soup.children)[3]
-ultag = soup.find('ul', {'class': 'list-group'})
-print(ultag)
-litag = ultag.find_all('li', {'class': 'list-group-item'})
-print(list(litag))
+for i in range(0, len(keys)):
+    fields[keys[i]] = 'p'+str(i)
+
+
+for event in data:
+    name = event[fields['eventName']]
+    print(name)
+    if name != 'False':
+        print(event[fields['clubName']])
+        dateTag = event[fields['eventDates']]
+        soup = BeautifulSoup('<b>' + str(dateTag) + '</b>', 'html.parser')
+        date = ''
+        for line in soup.findAll('p'):
+            date += line.getText() + ' '
+        print(date)
+        print(event[fields['eventLocation']])
+        print(event[fields['ariaEventDetailsWithLocation']])
+        print()
+
