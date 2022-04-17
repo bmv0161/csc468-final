cc0fbb6dc29b646ada227c69603b5a2003cde3f3
Brian Montecinos-Velazquez <bm935325@head.bmv.cloud-edu.emulab.net>
2022-04-17 15:47:49 -0600

    Connection webscraper->database working

---
+++ delete.sh
@@ -0,0 +1,7 @@
+kubectl delete all --all -n chatbot
+kubectl delete pvc --all -n chatbot
+kubectl delete pv mysql-pv -n chatbot
+kubectl delete sc mysql-sc -n chatbot
+kubectl delete namespace chatbot
+kubectl delete svc registry
+kubectl delete deployment registry
--- deploy_chatbot.sh
+++ deploy_chatbot.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 kubectl create deployment registry --image=registry
-kubectl expose deploy/registry --port=5555 --type=NodePort
+kubectl expose deploy/registry --port=5000 --type=NodePort
 kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
 
 docker-compose build
-docker-compose push
+webscraper/wait-for-it.sh 127.0.0.1:30000 -t 15 -- docker-compose push
 
 kubectl create namespace chatbot
 
