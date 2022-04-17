9f381f66af0fe01a2d8fd8ead7557ba9c4a1d1c7
Brian Montecinos-Velazquez <bm935325@head.bmv.cloud-edu.emulab.net>
2022-04-17 14:34:41 -0600

    kubernetes deployment of database works

--- deploy_chatbot.sh
+++ deploy_chatbot.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-kubectl create deployment registy --image=registry
-kubectel expose deploy/registry --port=30000 --type=NodePort
+kubectl create deployment registry --image=registry
+kubectl expose deploy/registry --port=5555 --type=NodePort
+kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
 
 docker-compose build
 docker-compose push
@@ -9,7 +10,7 @@ docker-compose push
 kubectl create namespace chatbot
 
 kubectl apply -f database/mysql-secret.yaml -n chatbot
-kubectl apply -f database/mysql-pv.yaml -n chatbot
+kubectl apply -f database/mysql-storage.yaml -n chatbot
 
 kubectl create -f chatbot-deployment.yaml --namespace chatbot
 kubectl create -f chatbot-services.yaml --namespace chatbot
