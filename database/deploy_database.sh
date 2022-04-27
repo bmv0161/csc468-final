db900aade36409fc4b23c639c55a815af67f6c38
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-04-27 19:38:32 -0400

    changed script for creating registry

--- database/deploy_database.sh
+++ database/deploy_database.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
-#create kubernetes registry
-kubectl create deployment registry --image=registry
-kubectl expose deploy/registry --port=5000 --type=NodePort
-kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
-docker-compose build
-
-#launch database volumes and pod
+#deploy database
 kubectl create namespace chatbot
 
 kubectl create -f mysql-secret.yaml -n chatbot
