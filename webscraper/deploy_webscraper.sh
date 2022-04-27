ca31786acf9a75120a0959c01f0c7a4b03319e1e
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-04-27 19:53:10 -0400

    webscraper kubernetes working

--- webscraper/deploy_webscraper.sh
+++ webscraper/deploy_webscraper.sh
@@ -5,7 +5,7 @@ kubectl create deployment registry --image=registry
 kubectl expose deploy/registry --port=5000 --type=NodePort
 kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
 docker-compose build
-wait-for-it.sh 127.0.0.1:30000 -t 15 -- docker-compose push
+./wait-for-it.sh 127.0.0.1:30000 -t 100 -- docker-compose push
 
 #deploy pod
 kubectl create namespace chatbot
