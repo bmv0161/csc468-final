54b81e09d6ef062576a268ab9bed3ec1dfd1a874
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-05-03 23:42:24 -0400

    chatbot kubernetes not working

--- app/deploy_chatbot.sh
+++ app/deploy_chatbot.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-kubectl create deployment registry --image=registry
-kubectl expose deploy/registry --port=5000 --type=NodePort
-kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
+#kubectl create deployment registry --image=registry
+#kubectl expose deploy/registry --port=5000 --type=NodePort
+#kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
 
-docker-compose build
+#docker-compose build
 webscraper/wait-for-it.sh 127.0.0.1:30000 -t 15 -- docker-compose push
 
 kubectl create namespace chatbot
