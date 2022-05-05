6a500b1b37f9405e43553e3aec52f38657390ad8
bmv0161 <91698838+bmv0161@users.noreply.github.com>
2022-05-04 22:18:48 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -40,8 +40,8 @@ for i in range(num_nodes):
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
 
   if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_jenkins.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/launch_jenkins.sh"))
   else:
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
     
