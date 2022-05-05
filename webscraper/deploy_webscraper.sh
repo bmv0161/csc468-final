5f9f56fadc284eb0f149dcca0956708ef3c462be
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-05-04 20:38:54 -0400

    updated files with registry port

--- webscraper/deploy_webscraper.sh
+++ webscraper/deploy_webscraper.sh
@@ -3,12 +3,12 @@
 #create kubernetes registry
 kubectl create deployment registry --image=registry
 kubectl expose deploy/registry --port=5000 --type=NodePort
-kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
+kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":35000}]'
 docker-compose build
-./wait-for-it.sh 127.0.0.1:30000 -t 100 -- docker-compose push
+./wait-for-it.sh 127.0.0.1:35000 -t 100 -- docker-compose push
 
 #deploy pod
-kubectl create namespace chatbot
+#kubectl create namespace chatbot
 
 kubectl create -f ../webscraper.yaml --namespace chatbot
 kubectl create -f ../webscraper_services.yaml --namespace chatbot
