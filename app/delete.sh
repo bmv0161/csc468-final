8b0f8d5ab461438eb53ea8dcff91aafde152317d
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-05-05 01:10:11 -0400

    cleaned up cloudlab branch

--- app/delete.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete all --all -n chatbot
-kubectl delete pvc --all -n chatbot
-kubectl delete pv mysql-pv -n chatbot
-kubectl delete sc mysql-sc -n chatbot
-kubectl delete namespace chatbot
-kubectl delete svc registry
-kubectl delete deployment registry
