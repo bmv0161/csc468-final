e479726063c8c83fda3909e1f0ba120c7c279879
bm935325 <bm935325@head.bmv.cloud-edu-pg0.clemson.cloudlab.us>
2022-05-04 20:28:35 -0400

    edit deploy script

--- database/deploy_database.sh
+++ database/deploy_database.sh
@@ -3,8 +3,8 @@
 #deploy database
 kubectl create namespace chatbot
 
-kubectl create -f mysql-secret.yaml -n chatbot
-kubectl create -f mysql-storage.yaml -n chatbot
+#kubectl create -f mysql-secret.yaml -n chatbot
+#kubectl create -f mysql-storage.yaml -n chatbot
 
 kubectl create -f ../database.yaml --namespace chatbot
 kubectl create -f ../database_services.yaml --namespace chatbot
