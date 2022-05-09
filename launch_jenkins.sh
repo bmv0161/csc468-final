8fd61155afcfc900a5aecf5c0f9d8d23b406667f
Brian Montecinos-Velazquez <91698838+bmv0161@users.noreply.github.com>
2022-05-09 17:14:13 -0400

    Update launch_jenkins.sh

--- launch_jenkins.sh
+++ launch_jenkins.sh
@@ -2,7 +2,7 @@
 
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
 sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=30000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
-sleep 90
+sleep 300
 kubectl create namespace jenkins
 kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
 kubectl -n kube-system create sa jenkins
